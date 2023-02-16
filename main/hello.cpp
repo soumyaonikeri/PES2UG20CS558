fc349e79ddf63f3782cdafb2c1716cf27eab8fc0
soumyaonikeri <115202691+soumyaonikeri@users.noreply.github.com>
2023-02-16 14:14:06 +0530

    Update hello.cpp
    commented a file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
- //added a webhook
+ //added a webhook to repo
   return 0;
 }
